6eec033861866daea700485dc26a216c9588635c
alexanderwnnr <alexander.wnnr@gmail.com>
2020-01-08 17:41:56 +0200

    add a-level 2 task

---
+++ alevel02.js
@@ -0,0 +1,11 @@
+const timer = function makeProfileTimer (someCode) {
+    let time = performance.now()
+    someCode
+    return time = performance.now() - time
+}
+
+function count(a, b) {    
+    return result = a + b  
+}
+
+alert.log(timer(count(2, 3)))
